8e4f37b81abda2bfa686c382435cc1b6f664301f
akash-kush9 <akash.kushwaha9@gmail.com>
2020-10-06 20:23:50 +0530

    base url set for fetching json for movie trailer

---
+++ src/axios.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+});
+
+export const BASE_URL = "https://image.tmdb.org/t/p/original";
+export default instance;
