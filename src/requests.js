d8a6ffdb5ecf61356928cc06e9a48e08eb23ab5d
akash-kush9 <akash.kushwaha9@gmail.com>
2020-10-07 22:29:40 +0530

    new url added for searching movie by id

--- src/requests.js
+++ src/requests.js
@@ -10,5 +10,6 @@ const requests = {
   fetch_Romance_Movies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
   fetch_Documentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
 };
-
+export const searchMovies = `/search/movie?api_key=${API_KEY}`;
+export const searchMovieById = `/movie/MOVIE_ID?api_key=${API_KEY}&language=en-US`;
 export default requests;
